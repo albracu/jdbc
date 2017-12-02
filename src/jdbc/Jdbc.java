ed29fc4c383c68e47540bdff268aef1d09f8b2cd
albracu <yo@alfredobravocuero.co>
2017-12-02 04:24:32 -0500

    Mostras datos de la BD en Java

--- src/jdbc/Jdbc.java
+++ src/jdbc/Jdbc.java
@@ -6,11 +6,23 @@ public class Jdbc {
     public static void main(String[] args) {
 
         try{
-            System.out.println("Intento conectarme a la BD....");
-            Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Intento conectarme a la BD....");
+        Class.forName("com.mysql.jdbc.Driver");
 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8889/agenda", "root","root");
-            System.out.println("La conexion se ha realizado con exito");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:8889/agenda", "root","root");
+        System.out.println("La conexion se ha realizado con exito");
+
+        // Devolver Agenda Telefonica
+
+        Statement estado = con.createStatement();
+        ResultSet resultado = estado.executeQuery("SELECT * FROM datos ORDER BY id");
+
+        System.out.println("ID \t Nombre \t Telefono");
+
+        while (resultado.next()){
+            System.out.println(resultado.getString("id")+"\t"+resultado.getString("nombre")+"\t \t"+resultado.getString("telefono"));
+        }
+
         }catch (SQLException ex){
             System.out.println("Error de Mysql");
 
